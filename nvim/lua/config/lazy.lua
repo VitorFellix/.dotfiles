a59abbfdb6f6b38ccaa685320ce7db33e0e6f985
Felix <vitor.felix@simepar.br>
2024-01-09 18:08:13 -0300

    fixed to be .dotfile

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -1,11 +1,11 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
 	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
+		'git',
+		'clone',
+		'--filter=blob:none',
+		'https://github.com/folke/lazy.nvim.git',
+		'--branch=stable', -- latest stable release
 		lazypath,
 	})
 end
@@ -13,38 +13,37 @@ vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
 
-	"folke/zen-mode.nvim",
-	"folke/neodev.nvim",
-	"nvim-tree/nvim-web-devicons", -- icons used by nerdfont
-	"williamboman/mason.nvim",
-	"williamboman/mason-lspconfig.nvim",
-	"theprimeagen/harpoon",
-	"tpope/vim-fugitive",
-	"folke/twilight.nvim", -- spotlight on the function that you are looking at, good on zenmode I guess?
-	"terrortylor/nvim-comment",
+	'folke/zen-mode.nvim',
+	'folke/neodev.nvim',
+	'nvim-tree/nvim-web-devicons', -- icons used by nerdfont
+	'williamboman/mason.nvim',
+	'williamboman/mason-lspconfig.nvim',
+	'theprimeagen/harpoon',
+	'tpope/vim-fugitive',
+	'folke/twilight.nvim', -- spotlight on the function that you are looking at, good on zenmode I guess?
+	'terrortylor/nvim-comment',
 
 	{
-		"rose-pine/neovim", -- colorscheme of primeagen
-		name = "rose-pine",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			vim.cmd.colorscheme('rose-pine-moon')
-		end
-	},
-	{
-		"EdenEast/nightfox.nvim", -- colorscheme <3
+		'rose-pine/neovim', -- colorscheme of primeagen
+		-- name = 'rose-pine',
 		-- lazy = false,
 		-- priority = 1000,
 		-- config = function()
-		--	-- carbonfox, dawnfox, terafox, nightfox
-		--		vim.cmd.colorscheme('terafox')
-		-- 	end
+		-- 	vim.cmd.colorscheme('rose-pine-moon')
+		-- end
+	},
+	{
+		'EdenEast/nightfox.nvim', -- colorscheme <3
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.cmd.colorscheme('nightfox')
+		end
 	},
 
 	{
-		"folke/which-key.nvim",
-		event = "VeryLazy",
+		'folke/which-key.nvim',
+		event = 'VeryLazy',
 		init = function()
 			vim.o.timeout = true
 			vim.o.timeoutlen = 300
@@ -52,15 +51,15 @@ local plugins = {
 	},
 
 	{
-		"nvim-treesitter/nvim-treesitter", -- better highlighting
-		build = ":TSUpdate",
+		'nvim-treesitter/nvim-treesitter', -- better highlighting
+		build = ':TSUpdate',
 	},
 
 	{
-		"nvim-telescope/telescope.nvim",
+		'nvim-telescope/telescope.nvim',
 		dependencies = { -- you can install ripgrep too (apt install ripgrep)
-			"nvim-lua/plenary.nvim",
-			"sharkdp/fd", -- a opensource version of ´find´
+			'nvim-lua/plenary.nvim',
+			'sharkdp/fd', -- a opensource version of ´find´
 		}
 	},
 
@@ -87,4 +86,4 @@ local plugins = {
 
 }
 
-require("lazy").setup(plugins, {})
+require('lazy').setup(plugins, {})
