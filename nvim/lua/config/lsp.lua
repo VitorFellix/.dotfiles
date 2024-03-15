d29457bc66115589d9b889783caaa5fd4fe6948e
Felix <vitor.felix@simepar.br>
2024-03-15 12:15:18 -0300

    fix: formating " to \' add: correct lua lsp config

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -1,21 +1,81 @@
-local lsp_zero = require('lsp-zero')
+-- Setup language servers.
 
-lsp_zero.on_attach(function(client, bufnr)
-	-- see :help lsp-zero-keybindings
-	-- to learn the available actions
-	lsp_zero.default_keymaps({ buffer = bufnr })
-
-	vim.keymap.set('n', 'gD', vim.lsp.buf.declaration)
-	vim.keymap.set('n', 'gd', vim.lsp.buf.definition)
-end)
-
-require('mason').setup({})
-require('mason-lspconfig').setup({
-	handlers = {
-		lsp_zero.default_setup,
-	},
+require("mason").setup({
+	ui = {
+		icons = {
+			package_installed = "✓",
+			package_pending = "➜",
+			package_uninstalled = "✗"
+		}
+	}
 })
+require("mason-lspconfig").setup()
 
-local lua_opts = lsp_zero.nvim_lua_ls()
+local lspconfig = require 'lspconfig'
+local config = require 'lspconfig.configs'
 
-require('lspconfig').lua_ls.setup(lua_opts)
+lspconfig.lua_ls.setup {
+	on_init = function(client)
+		local path = client.workspace_folders[1].name
+		if vim.loop.fs_stat(path .. '/.luarc.json') or vim.loop.fs_stat(path .. '/.luarc.jsonc') then
+			return
+		end
+
+		client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+			runtime = {
+				version = 'LuaJIT'
+			},
+			workspace = {
+				checkThirdParty = false,
+				library = {
+					vim.env.VIMRUNTIME
+				}
+			}
+		})
+	end,
+	settings = {
+		Lua = {}
+	},
+	diagnostics = {
+		globals = {'vim'}
+	}
+}
+
+
+-- Global mappings.
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+
+-- Use LspAttach autocommand to only map the following keys
+-- after the language server attaches to the current buffer
+vim.api.nvim_create_autocmd('LspAttach', {
+	group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+	callback = function(ev)
+		-- Enable completion triggered by <c-x><c-o>
+		vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+		-- Buffer local mappings.
+		-- See `:help vim.lsp.*` for documentation on any of the below functions
+		local opts = { buffer = ev.buf }
+		vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+		vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+		vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+		vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+		vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+		vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+		vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+		vim.keymap.set('n', '<space>wl', function()
+			print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+		end, opts)
+		vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+		vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+		vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+		vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+		vim.keymap.set('n', '<space>f', function()
+			vim.lsp.buf.format { async = true }
+		end, opts)
+	end,
+})
--- nvim/lua/config/treesitter.lua
+++ nvim/lua/config/treesitter.lua
@@ -1,5 +1,5 @@
 require 'nvim-treesitter.configs'.setup {
-	ensure_installed = { "lua", "vim", "vimdoc", "javascript", "html", "java", "scala", "go" },
+	ensure_installed = { "lua", "vim", "vimdoc", "javascript", "html", "java", "scala", "go", "css" },
 	indent = { enable = true },
 	sync_install = false,
 	auto_install = true,
