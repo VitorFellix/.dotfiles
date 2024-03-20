a9b3662763baf44b6f772eb883bc4eb3c2e5bd75
Vitor Felix <vitor_fellix@hotmail.com>
2024-03-20 15:36:06 -0300

    add: config folder

--- setup.sh
+++ setup.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
 echo 'installing dependencies'
-sudo apt install zsh tmux 
+sudo apt install zsh tmux git curl terminator
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" # install ohmyzsh
 chsh -s $(which zsh) # sets zsh as the default shell
+
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 rm -rf ~/.config/nvim
 ln -s ~/.dotfiles/nvim ~/.config/nvim
 echo 'added link for ~/.config/nvim'
 
-rm -rf ~/.zshrc
-ln -s ~/.dotfiles/.zshrc ~/.zshrc
+rm ~/.zshrc
+ln -s ~/.dotfiles/config/.zshrc ~/.zshrc
 echo 'added link for ~/.zshrc'
 
-rm -rf ~/.tmux.conf
-ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
+rm ~/.tmux.conf
+ln -s ~/.dotfiles/config/.tmux.conf ~/.tmux.conf
 echo 'added link for ~/.tmux.conf'
 
+rm ~/.config/terminator/config
+ln -s ~/.dotfiles/config/terminator ~/.config/terminator/config
+echo 'added link for ~/.config/terminator/config'
+
+echo 'install plugins inside tmux with: <C-b> I' 
 echo 'finished'
