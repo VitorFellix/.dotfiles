db1bbe3bfbe7a22e6ce632245b5165754c8be488
Vitor Felix da Silva <vitor.fellix@hotmail.com>
2024-10-19 15:34:34 -0300

    Fix: install packages script

--- setup/install-pkgs.sh
+++ setup/install-pkgs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo 'installing packages'
-yum install tmux kitty curl git i3
+yum install tmux i3 neovim curl git
 
 # sets bash as the default shell 
 chsh -s $(which bash)
@@ -9,13 +9,22 @@ chsh -s $(which bash)
 # TODO: check if path already exists before cloning
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-rm -rf ~/.config/nvim
-ln -s ~/.dotfiles/nvim ~/.config/nvim
-echo 'added link for ~/.config/nvim'
+echo '[WARN] watch out! this script will remove your .bashrc file'
+rm ~/.bashrc
+ln -s ~/.dotfiles/config/bashrc.config ~/.bashrc
+echo 'added link for bash'
 
 rm ~/.tmux.conf
-ln -s ~/.dotfiles/config/.tmux.conf ~/.tmux.conf
-echo 'added link for ~/.tmux.conf'
+ln -s ~/.dotfiles/config/tmux.config ~/.tmux.conf
+echo 'added link for tmux'
+
+rm -rf ~/.config/i3/config
+ln -s ~/.dotfiles/config/i3.config ~/.config/i3/config
+echo 'added link for i3'
+
+rm -rf ~/.config/nvim
+ln -s ~/.dotfiles/nvim ~/.config/nvim
+echo 'added link for nvim'
 
 # rm ~/.config/terminator/config
 # ln -s ~/.dotfiles/config/terminator ~/.config/terminator/config
