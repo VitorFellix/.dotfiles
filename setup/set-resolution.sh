5866b24e17295e16dcd2d435dab0958de110db82
Vitor <vitor_fellix@hotmail.com>
2024-10-31 12:52:58 -0300

    Fix: dpi setting and hostname typo

--- setup/set-resolution.sh
+++ setup/set-resolution.sh
@@ -1,34 +1,42 @@
 #!/bin/bash
+set -e
 
-# variables :)
 HOSTNAME_PERSONAL=fedora
-HOSTNAME_WORK=sim996
+HOSTNAME_WORK=sim2996
 
 PRIMARY_MONITOR=$(xrandr | grep connected | grep -v disconnected | awk '{print $1}' | head -n 1)
 SECONDARY_MONITOR=$(xrandr | grep connected | grep -v disconnected | awk '{print $1}' | head -n 2 | tail -n 1)
 
-# run xrandr
 if [[ $(hostname) == "$HOSTNAME_PERSONAL" ]]; then
 	echo "Detected=[Personal-Computer]"
-	echo "Xft.dpi: 120" > ~/.Xresources
+	echo "Xft.dpi: 92" > ~/.Xresources
+	xrdb -merge ~/.Xresources
+	cat ~/.Xresources
 	
-	xrandr --output $PRIMARY_MONITOR --auto --above $SECONDARY_MONITOR --output $SECONDARY_MONITOR --auto
-else
+	if [[ $PRIMARY_MONITOR == "DP-0" ]]; then
+		echo "Monitor=[${PRIMARY_MONITOR}]"
+		xrandr --output $PRIMARY_MONITOR --auto --dpi 92
+	fi
+	
+	# maybe the dpi is 92 too, must test
+	if [[ $SECONDARY_MONITOR == "HDMI-1" ]]; then
+		echo "Monitor=[${SECONDARY_MONITOR}]"
+		xrandr --output $SECONDARY_MONITOR --auto --below $PRIMARY_MONITOR --dpi 81
+	fi
+	
+elif [[ $(hostname) == "$HOSTNAME_WORK" ]]; then
 	echo "Detected=[Work-Computer]"
-	echo "Xft.dpi: 192" > ~/.Xresources
+	echo "Xft.dpi: 162" > ~/.Xresources
+	cat ~/.Xresources
+	xrdb -merge ~/.Xresources
 	
-	xrandr --output $PRIMARY_MONITOR --auto --left-of $SECONDARY_MONITOR --output $SECONDARY_MONITOR --auto
+	if [[ $PRIMARY_MONITOR == "eDP" ]]; then
+		echo "Monitor=[${PRIMARY_MONITOR}]"
+		xrandr --output $PRIMARY_MONITOR --auto --dpi 242 --scale 1
+	fi
 	
-	# if [[ $PRIMARY_MONITOR == "eDP" ]]; then
-	# 	echo "Monitor=[${PRIMARY_MONITOR}]"
-	# 	xrandr --output eDP --scale 1 --filter nearest --mode 2880x1800 --refresh 90 
-	# fi
-	#
-	# if [[ $SECONDARY_MONITOR == "DisplayPort-1" ]]; then
-	# 	echo "Monitor=[${SECONDARY_MONITOR}]"
-	# 	xrandr --output DisplayPort-1 --scale 1 --filter nearest --mode 3840x2160 --refresh 60;
-	# fi
+	if [[ $SECONDARY_MONITOR == "DisplayPort-1" ]]; then
+		echo "Monitor[${SECONDARY_MONITOR}]"
+		xrandr --output $SECONDARY_MONITOR --auto --right-of $PRIMARY_MONITOR --dpi 162
+	fi
 fi
-
-# merge?
-xrdb -merge ~/.Xresources
