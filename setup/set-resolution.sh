800dc153aff22825b6d93a56c7a049fdc3beb712
Vitor Felix da Silva <vitor.fellix@hotmail.com>
2024-11-03 23:39:20 -0300

    Update: changed keybindings Fix: resolution script Add: wallpaper script

--- setup/set-resolution.sh
+++ setup/set-resolution.sh
@@ -7,36 +7,39 @@ HOSTNAME_WORK=sim2996
 PRIMARY_MONITOR=$(xrandr | grep connected | grep -v disconnected | awk '{print $1}' | head -n 1)
 SECONDARY_MONITOR=$(xrandr | grep connected | grep -v disconnected | awk '{print $1}' | head -n 2 | tail -n 1)
 
+DPI1=120
+DPI2=162
+
 if [[ $(hostname) == "$HOSTNAME_PERSONAL" ]]; then
 	echo "Detected=[Personal-Computer]"
-	echo "Xft.dpi: 92" > ~/.Xresources
+	echo "Xft.dpi: $DPI1" > ~/.Xresources
 	xrdb -merge ~/.Xresources
 	cat ~/.Xresources
 	
 	if [[ $PRIMARY_MONITOR == "DP-0" ]]; then
 		echo "Monitor=[${PRIMARY_MONITOR}]"
-		xrandr --output $PRIMARY_MONITOR --auto --dpi 92
+		xrandr --output $PRIMARY_MONITOR --auto --dpi $DPI1 --primary
 	fi
 	
 	# maybe the dpi is 92 too, must test
 	if [[ $SECONDARY_MONITOR == "HDMI-1" ]]; then
 		echo "Monitor=[${SECONDARY_MONITOR}]"
-		xrandr --output $SECONDARY_MONITOR --auto --below $PRIMARY_MONITOR --dpi 81
+		xrandr --output $SECONDARY_MONITOR --auto --below $PRIMARY_MONITOR --dpi $DPI1
 	fi
-	
+	echo "$PRIMARY_MONITOR $SECONDARY_MONITOR" > ~/.test
 elif [[ $(hostname) == "$HOSTNAME_WORK" ]]; then
 	echo "Detected=[Work-Computer]"
-	echo "Xft.dpi: 162" > ~/.Xresources
+	echo "Xft.dpi: $DPI2" > ~/.Xresources
 	cat ~/.Xresources
 	xrdb -merge ~/.Xresources
 	
 	if [[ $PRIMARY_MONITOR == "eDP" ]]; then
 		echo "Monitor=[${PRIMARY_MONITOR}]"
-		xrandr --output $PRIMARY_MONITOR --auto --dpi 242 --scale 1
+		xrandr --output $PRIMARY_MONITOR --auto --dpi $DPI2 --primary
 	fi
 	
 	if [[ $SECONDARY_MONITOR == "DisplayPort-1" ]]; then
 		echo "Monitor[${SECONDARY_MONITOR}]"
-		xrandr --output $SECONDARY_MONITOR --auto --right-of $PRIMARY_MONITOR --dpi 162
+		xrandr --output $SECONDARY_MONITOR --auto --right-of $PRIMARY_MONITOR --dpi $DPI2
 	fi
 fi
