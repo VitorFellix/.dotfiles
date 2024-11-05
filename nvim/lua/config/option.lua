7310ec25095be82a19e60300677a61fb0086527f
Vitor <vitor_fellix@hotmail.com>
2024-11-05 13:32:04 -0300

    Update: workspaces, modifiers keys, added nvim nordic theme and remove old

--- nvim/lua/config/option.lua
+++ nvim/lua/config/option.lua
@@ -15,7 +15,7 @@ vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.smartindent = true
 
-vim.opt.signcolumn = "no"
+vim.opt.signcolumn = "yes"
 vim.opt.wrap = false
 vim.opt.scrolloff = 12
 vim.opt.colorcolumn = "120"
