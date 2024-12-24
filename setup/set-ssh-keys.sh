2182c93901973a64dde808493d6d2b2aa6541045
Vitor <vitor_fellix@hotmail.com>
2024-12-24 12:34:40 -0300

    feature: proper key search on set-ssh-keys script

--- setup/set-ssh-keys.sh
+++ setup/set-ssh-keys.sh
@@ -1,1 +1,14 @@
-ssh-add ~/.ssh/*
+#!/bin/bash
+
+ssh_dir=${HOME}/.ssh
+
+if [ ! -d ${ssh_dir} ]; then
+	echo "${ssh_dir} doesnt exists"
+	exit 1
+fi
+
+keys=$(find ${ssh_dir} -type f | grep -v "pub\|old\|known\|host")
+
+for k in ${keys}; do
+	ssh-add ${k}
+done
