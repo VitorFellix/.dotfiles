3797afed2aad77f9dbf492ddd4d29922e9913ec5
Vitor Felix da Silva <vitor_fellix@hotmail.com>
2025-03-24 22:39:05 -0300

    fix: log messages on install-pkgs.sh

--- setup/install-pkgs.sh
+++ setup/install-pkgs.sh
@@ -1,29 +1,34 @@
 #!/bin/bash
 
-PKGS="tmux neovim redshift btop git ranger fastfetch"
+log_info() {
+	echo "INFO: $1"
+}
 
-echo '[EXEC] installing packages'
+log_info 'Install packages script.'
+PKGS="tmux neovim redshift btop git fastfetch"
 
-echo "[EXEC] \$ sudo dnf install ${PKGS}"
+log_info " \$ sudo dnf install ${PKGS}"
 sudo dnf install ${PKGS}
-echo "[DONE] \$ $PKG_MANAGER install"
+log_info " \$ $PKG_MANAGER install"
+
+if [[ ! -d ~/.tmux/plugins/tpm ]]; then
+	git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
 
 read -p "Install flatpak packages: [y/n] " ANSWER
 if [[ $ANSWER == "y" ]]; then
 	sudo dnf install -y flatpak
-	echo "[EXEC] \$ flatpak install"
 	flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 	flatpak install -y com.spotify.Client com.discordapp.Discord md.obsidian.Obsidian com.google.Chrome
-	echo "[DONE] \$ flatpak install"
 fi
 
 read -p "Install betterlockscreen and i3lock-color: [y/n] " ANSWER
 if [[ $ANSWER == "y" ]]; then
 
-	echo "[RUN] installing dependencies"
+	log_info "Installing dependencies"
 	sudo dnf install -y autoconf automake cairo-devel fontconfig gcc libev-devel libjpeg-turbo-devel libXinerama libxkbcommon-devel libxkbcommon-x11-devel libXrandr pam-devel pkgconf xcb-util-image-devel xcb-util-xrm-devel xdpyinfo bc xset
  	
-	echo "[RUN] installing i3lock-color"
+	log_info "Installing i3lock-color"
 	git clone https://github.com/Raymo111/i3lock-color.git /tmp/i3lock-color
 	cd /tmp/i3lock-color
 	if [[ -f install-i3lock-color.sh ]]; then
@@ -31,9 +36,9 @@ if [[ $ANSWER == "y" ]]; then
 	fi
 	cd -
 	
- 	echo "[RUN] installing betterlockscreen"
+ 	log_info "Installing betterlockscreen"
 	wget https://raw.githubusercontent.com/betterlockscreen/betterlockscreen/main/install.sh -O - -q | bash -s user
 fi
 
-echo '[DONE] installing packages'
+log_info 'finished'
 exit 0
