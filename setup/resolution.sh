ce4e12d483009d09ff814ebb91c95b9afa6fb363
Vitor Felix da Silva <vitor_fellix@hotmail.com>
2025-03-26 20:08:28 -0300

    fix: for home computer

--- setup/resolution.sh
+++ setup/resolution.sh
@@ -12,24 +12,20 @@ DPI2=162
 
 if [[ $(hostname) == "$HOSTNAME_PERSONAL" ]]; then
 	echo "Detected=[Personal-Computer]"
-	echo "Xft.dpi: $DPI1" > ~/.Xresources
+
+	cp ~/.dotfiles/config/.Xresources ~/.Xresources
 	xrdb -merge ~/.Xresources
 	cat ~/.Xresources
+
+	CMD_MONITOR="xrandr --output $PRIMARY_MONITOR --mode 3840x2160 --refresh 239.99 --dpi 137 --primary"
 	
-	if [[ $PRIMARY_MONITOR == "DP-1" ]]; then
-		echo "Monitor=[${PRIMARY_MONITOR}]"
-		xrandr --output $PRIMARY_MONITOR --auto --dpi $DPI1 --primary
-	fi
-	
-	# maybe the dpi is 92 too, must test
-	if [[ $SECONDARY_MONITOR == "HDMI-2" ]]; then
-		echo "Monitor=[${SECONDARY_MONITOR}]"
-		xrandr --output $SECONDARY_MONITOR --auto --below $PRIMARY_MONITOR --dpi $DPI1
-	fi
-	echo "$PRIMARY_MONITOR $SECONDARY_MONITOR" > ~/.test
+	echo $CMD_MONITOR
+
+	eval $CMD_MONITOR
+
 elif [[ $(hostname) == "$HOSTNAME_WORK" ]]; then
 	echo "Detected=[Work-Computer]"
-	echo "Xft.dpi: $DPI2" > ~/.Xresources
+
 	cat ~/.Xresources
 	xrdb -merge ~/.Xresources
 	
