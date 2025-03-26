5e063035ddd31e929aefd3547cb442770a19ef6a
Vitor Felix da Silva <vitor_fellix@hotmail.com>
2025-03-26 20:10:40 -0300

    update: wezterm config

--- config/wezterm.lua
+++ config/wezterm.lua
@@ -3,14 +3,15 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 config = {
-	-- color_scheme = 'Andromeda',
+	color_scheme = 'rose-pine-moon',
+	font = wezterm.font('IosevkaTerm Nerd Font Mono', { weight = 'Regular', italic = false }),
 
 	use_fancy_tab_bar = false,
 	tab_max_width = 40,
 	hide_tab_bar_if_only_one_tab = true,
 
 	line_height = 1.0,
-	font_size = 10.0,
+	font_size = 13.0,
 
 	use_resize_increments = false,
 	window_padding = {
