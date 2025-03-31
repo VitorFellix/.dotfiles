e0dbb3fa01bb084afbb0c6267dfa5ebf696055b7
Vitor Felix da Silva <vitor_fellix@hotmail.com>
2025-03-31 14:47:11 -0300

    feat: menu apps on polybar. feat: dinamicaly settings primary bar based on monitors

--- setup/polybar.sh
+++ setup/polybar.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
-killall -g polybar
+killall -q polybar
 
-polybar -r primary &
-polybar -r secondary &
+# Wait for process to shut down
+while pgrep -x polybar >/dev/null; do sleep 1; done
+
+screens=$(xrandr --listactivemonitors | grep -v "Monitors" | cut -d" " -f6)
+
+if [[ $(xrandr --listactivemonitors | grep -v "Monitors" | cut -d" " -f4 | cut -d"+" -f2- | uniq | wc -l) == 1 ]]; then
+  MONITOR=$(polybar --list-monitors | cut -d":" -f1) TRAY_POS="pulseaudio date" polybar -r primary &
+else
+  MONITOR_PRIMARY=$(xrandr --query | grep primary | cut -d" " -f1)
+
+  for m in $screens; do
+    if [[ $MONITOR_PRIMARY == $m ]]; then
+        MONITOR=$m TRAY_POS="pulseaudio date" polybar -r primary &
+    else
+        MONITOR=$m TRAY_POS="cpu memory filesystem" polybar -r secondary &
+    fi
+  done
+fi
