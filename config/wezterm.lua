cc9d9af596dfd50e4d10e1a9fcf82800f800762b
Vitor Felix da Silva <vitor_fellix@hotmail.com>
2025-03-31 16:54:29 -0300

    update: changed picom, i3 and wezterm themes

--- config/wezterm.lua
+++ config/wezterm.lua
@@ -5,21 +5,23 @@ local config = wezterm.config_builder()
 config = {
 	-- color_scheme = 'rose-pine-moon',
 	color_scheme = 'Kanagawa (Gogh)',
-	font = wezterm.font('IosevkaTerm Nerd Font Mono', { weight = 'Regular', italic = false }),
+
+	-- font = wezterm.font('IosevkaTerm Nerd Font Mono', { weight = 'Regular', italic = false }),
+	font = wezterm.font('DepartureMono Nerd Font', { weight = 'Regular', italic = false }),
 
 	use_fancy_tab_bar = false,
 	tab_max_width = 40,
 	hide_tab_bar_if_only_one_tab = true,
 
 	line_height = 1.0,
-	font_size = 13.0,
+	font_size = 12.0,
 
-	use_resize_increments = false,
+	use_resize_increments = true,
 	window_padding = {
-		left = 4,
-		right = 4,
-		top = 4,
-		bottom = 4,
+		left = 6,
+		right = 6,
+		top = 6,
+		bottom = 6,
 	}
 }
 
