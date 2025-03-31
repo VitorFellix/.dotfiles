447e254040bd3b0994192f0ffd6619ce18d724e1
Vitor Felix da Silva <vitor_fellix@hotmail.com>
2025-03-31 17:12:19 -0300

    fix: added autcmd for setting the background color equal to terminal

--- nvim/lua/config/autocmd.lua
+++ nvim/lua/config/autocmd.lua
@@ -1,7 +1,17 @@
+-- Highlight when yanking
 vim.api.nvim_create_autocmd("TextYankPost", {
 	desc = "Highlight when yanking text",
 	group = vim.api.nvim_create_augroup("highligh-yank", { clear = true }),
 	callback = function()
 		vim.highlight.on_yank()
 	end,
+})
+
+-- Terminal background fix
+vim.api.nvim_create_autocmd("ColorScheme", {
+  callback = function()
+    -- This sets the terminal background to match Neovim's background
+    vim.api.nvim_set_hl(0, "Normal", { bg = "NONE" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE" })
+  end,
 })
