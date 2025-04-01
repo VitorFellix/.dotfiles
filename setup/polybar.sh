0c7daf1e9356f58e039cc3a005d0a065d7d3fc79
Vitor Felix da Silva <vitor_fellix@hotmail.com>
2025-04-01 17:30:04 -0300

    feat: added dpi variable to polybar

--- setup/polybar.sh
+++ setup/polybar.sh
@@ -8,15 +8,15 @@ while pgrep -x polybar >/dev/null; do sleep 1; done
 screens=$(xrandr --listactivemonitors | grep -v "Monitors" | cut -d" " -f6)
 
 if [[ $(xrandr --listactivemonitors | grep -v "Monitors" | cut -d" " -f4 | cut -d"+" -f2- | uniq | wc -l) == 1 ]]; then
-  MONITOR=$(polybar --list-monitors | cut -d":" -f1) polybar -r primary &
+  MONITOR=$(polybar --list-monitors | cut -d":" -f1) DPI=137 polybar -r primary &
 else
   MONITOR_PRIMARY=$(xrandr --query | grep primary | cut -d" " -f1)
 
   for m in $screens; do
     if [[ $MONITOR_PRIMARY == $m ]]; then
-        MONITOR=$m polybar -r primary &
+        MONITOR=$m DPI=162 polybar -r primary &
     else
-        MONITOR=$m polybar -r secondary &
+        MONITOR=$m DPI=242 polybar -r secondary &
     fi
   done
 fi
