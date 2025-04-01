a063e7fe6720adff57d3cabb513ab3b3341d7170
Vitor Felix da Silva <vitor_fellix@hotmail.com>
2025-04-01 10:43:41 -0300

    update: changed defaults for polybar

--- setup/polybar.sh
+++ setup/polybar.sh
@@ -8,15 +8,15 @@ while pgrep -x polybar >/dev/null; do sleep 1; done
 screens=$(xrandr --listactivemonitors | grep -v "Monitors" | cut -d" " -f6)
 
 if [[ $(xrandr --listactivemonitors | grep -v "Monitors" | cut -d" " -f4 | cut -d"+" -f2- | uniq | wc -l) == 1 ]]; then
-  MONITOR=$(polybar --list-monitors | cut -d":" -f1) TRAY_POS="pulseaudio date" polybar -r primary &
+  MONITOR=$(polybar --list-monitors | cut -d":" -f1) polybar -r primary &
 else
   MONITOR_PRIMARY=$(xrandr --query | grep primary | cut -d" " -f1)
 
   for m in $screens; do
     if [[ $MONITOR_PRIMARY == $m ]]; then
-        MONITOR=$m TRAY_POS="pulseaudio date" polybar -r primary &
+        MONITOR=$m polybar -r primary &
     else
-        MONITOR=$m TRAY_POS="cpu memory filesystem" polybar -r secondary &
+        MONITOR=$m polybar -r secondary &
     fi
   done
 fi
