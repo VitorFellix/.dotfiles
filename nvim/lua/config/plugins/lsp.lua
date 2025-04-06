26ab8e8dcfc0461ea18df635d3f7e2c010a25e43
Vitor Felix da Silva <vitor_fellix@hotmail.com>
2025-04-06 17:38:39 -0300

    feat: used AI to generate and fix my lsp config for neovim...

--- nvim/lua/config/plugins/lsp.lua
+++ nvim/lua/config/plugins/lsp.lua
@@ -1,105 +1,127 @@
 return {
 	"neovim/nvim-lspconfig",
 	dependencies = {
+		-- LSP setup
 		"williamboman/mason.nvim",
 		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+
+		-- Java support
 		"nvim-java/nvim-java",
-		"hrsh7th/cmp-nvim-lsp",
-		"hrsh7th/cmp-buffer",
-		"hrsh7th/cmp-path",
-		"hrsh7th/cmp-cmdline",
-		"hrsh7th/nvim-cmp",
 		"nvim-java/lua-async-await",
 		"nvim-java/nvim-java-refactor",
 		"nvim-java/nvim-java-core",
 		"nvim-java/nvim-java-test",
 		"nvim-java/nvim-java-dap",
-		"mfussenegger/nvim-dap",
-		"MunifTanjim/nui.nvim",
+
+		-- Completion
+		"hrsh7th/nvim-cmp",
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-cmdline",
 		"L3MON4D3/LuaSnip",
 		"saadparwaiz1/cmp_luasnip",
+
+		-- Extra utilities
+		"mfussenegger/nvim-dap",
 		"j-hui/fidget.nvim",
-		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		"MunifTanjim/nui.nvim",
 		{ "folke/neodev.nvim", opts = {} },
+
+		-- Formatting/Linting
+		"nvimtools/none-ls.nvim", -- null-ls successor
 	},
 	config = function()
-		local cmp = require('cmp')
+		local cmp = require("cmp")
 		local cmp_lsp = require("cmp_nvim_lsp")
 		local capabilities = vim.tbl_deep_extend(
 			"force",
 			{},
 			vim.lsp.protocol.make_client_capabilities(),
-			cmp_lsp.default_capabilities())
+			cmp_lsp.default_capabilities()
+		)
 
-		require("java").setup({})
+		-- Java setup
+		require("java").setup({
+			spring_boot_tools = {
+				enabled = true,
+			},
+		})
+
 		require("fidget").setup({})
 		require("mason").setup({})
+		-- used for lsp, linter and formatters
+		require("mason-tool-installer").setup({
+			ensure_installed = {
+				"prettier", "stylua", "gopls", "goimports", "shfmt", "typescript-language-server", "html-lsp",
+				"css-lsp", "json-lsp", "lua-language-server", "bash-language-server", "gopls",
+				"dockerfile-language-server", "lemminx", "grammarly-languageserver",
+			},
+			auto_update = true,
+		})
+		-- used for configuring lsp
 		require("mason-lspconfig").setup({
 			automatic_installation = true,
 			ensure_installed = {
-				"grammarly",
-				"jsonls",
-				"lemminx",
-				"intelephense",
 				"lua_ls",
-				"dockerls",
+				"jsonls",
 				"bashls",
+				"gopls",
+				"html",
+				"cssls",
+				"dockerls",
+				"lemminx",
+				"grammarly",
 			},
 			handlers = {
-				function(server_name) -- default handler (optional)
-					require("lspconfig")[server_name].setup {
-						capabilities = capabilities
-					}
-				end,
-				["markdown_oxide"] = function()
-					capabilities.workspace = {
-						didChangeWatchedFiles = {
-							dynamicRegistration = true,
-						},
-					}
+				function(server_name)
+					require("lspconfig")[server_name].setup({
+						capabilities = capabilities,
+					})
 				end,
 				["lua_ls"] = function()
-					local lspconfig = require("lspconfig")
-					lspconfig.lua_ls.setup {
+					require("lspconfig").lua_ls.setup({
 						capabilities = capabilities,
 						settings = {
 							Lua = {
 								runtime = { version = "Lua 5.1" },
 								diagnostics = {
 									globals = { "vim", "it", "describe", "before_each", "after_each" },
-								}
-							}
-						}
-					}
+								},
+							},
+						},
+					})
 				end,
-			}
+			},
 		})
 
+		-- Completion setup
 		local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
 		cmp.setup({
 			snippet = {
 				expand = function(args)
-					require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+					require("luasnip").lsp_expand(args.body)
 				end,
 			},
 			mapping = cmp.mapping.preset.insert({
-				['<C-k>'] = cmp.mapping.select_prev_item(cmp_select),
-				['<C-j>'] = cmp.mapping.select_next_item(cmp_select),
-				['<C-l>'] = cmp.mapping.confirm({ select = true }),
+				["<C-k>"] = cmp.mapping.select_prev_item(cmp_select),
+				["<C-j>"] = cmp.mapping.select_next_item(cmp_select),
+				["<C-l>"] = cmp.mapping.confirm({ select = true }),
 				["<C-Space>"] = cmp.mapping.complete(),
 			}),
 			sources = cmp.config.sources({
-				{ name = 'nvim_lsp' },
-				{ name = 'luasnip' }, -- For luasnip users.
+				{ name = "nvim_lsp" },
+				{ name = "luasnip" },
 			}, {
-				{ name = 'buffer' },
-			})
+				{ name = "buffer" },
+			}),
 		})
 
+		-- Diagnostic visuals
 		vim.diagnostic.config({
 			virtual_text = true,
-			-- update_in_insert = true,
 			float = {
 				source = true,
 				focusable = false,
@@ -109,5 +131,24 @@ return {
 				prefix = "",
 			},
 		})
-	end
+
+		-- Formatter / Linter Setup
+		local null_ls = require("null-ls")
+		null_ls.setup({
+			sources = {
+				-- Web: HTML, CSS, JS, TS, JSON
+				null_ls.builtins.formatting.prettier,
+
+				-- Lua
+				null_ls.builtins.formatting.stylua,
+
+				-- Go
+				null_ls.builtins.formatting.gofmt,
+				null_ls.builtins.formatting.goimports,
+
+				-- Bash
+				null_ls.builtins.formatting.shfmt,
+			},
+		})
+	end,
 }
