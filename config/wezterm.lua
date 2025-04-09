76d1d36ee46e3ed38fb5daaf0c7b04ba4546b574
Vitor Felix da Silva <vitor_fellix@hotmail.com>
2025-04-08 21:02:41 -0300

    update: wezterm theme

--- config/wezterm.lua
+++ config/wezterm.lua
@@ -3,7 +3,7 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 config = {
-	color_scheme = 'Kanagawa (Gogh)',
+	color_scheme = 'One Dark (Gogh)',
 
 	font = wezterm.font('JetBrainsMono Nerd Font', { weight = 'Regular', italic = false }),
 
@@ -12,7 +12,7 @@ config = {
 	hide_tab_bar_if_only_one_tab = true,
 
 	line_height = 1.0,
-	font_size = 12.0,
+	font_size = 10.0,
 
 	use_resize_increments = true,
 	window_padding = {
