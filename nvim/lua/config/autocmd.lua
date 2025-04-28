594c6d7cfbf0106581eb421d41f43935d4291620
Vitor Felix da Silva <vitor_fellix@hotmail.com>
2025-04-28 17:44:11 -0300

    remove: same color as terminal background for neovim

--- nvim/lua/config/autocmd.lua
+++ nvim/lua/config/autocmd.lua
@@ -6,12 +6,3 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 		vim.highlight.on_yank()
 	end,
 })
-
--- Terminal background fix
-vim.api.nvim_create_autocmd("ColorScheme", {
-  callback = function()
-    -- This sets the terminal background to match Neovim's background
-    vim.api.nvim_set_hl(0, "Normal", { bg = "NONE" })
-    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE" })
-  end,
-})
