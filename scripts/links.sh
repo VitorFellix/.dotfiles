b6023195275d0651fe4c265f860a649db0d31cf4
Vitor Felix <vitor_fellix@hotmail.com>
2025-05-25 22:47:20 -0300

    fix: links script updated

--- scripts/links.sh
+++ scripts/links.sh
@@ -1,50 +1,45 @@
 #!/bin/bash
+# This script creates a link for each config from the .dotfiles folder to .config
 
-log_info() {
-	echo "INFO: $1"
-}
-
-log_warn() {
-	echo "WARN: $1"
-}
-
-log_info 'Set links script.'
-
-# $1 is link name
-# $2 is path to .config
-# $3 is path to config inside .dotfile/config
 createLink() {
-	local hook=$4
+	# $1 link name
+	local LINK_NAME=$1
+	# $2 path to .config
+	local DOT_CONFIG=$2
+	# $3 path to config inside .dotfile/config
+	local DOT_FILE_CONFIG=$3
+	# $4 hook string, can be used to run other commands before creating the link
+	local HOOK=$4
 
-	# log_info "Should link configuration for $1"
-	log_info "Configuring $1..."
-	# read -p "[y/n] " ANSWER
-	# if [[ $ANSWER == "y" ]]; then
-		eval "rm -rf $2"
-		eval $hook
-		eval "ln -s $3 $2"
-		log_info "Configuration for $1 finished." 
-	# fi
+	log "configuring $1..."
+	eval "rm -rf $2"
+	eval $HOOK
+	eval "ln -s $3 $2"
+	log "configuration for $1 finished." 
 }
 
+log '--- default x11 and bashrc configs'
 createLink "bashrc" "~/.bashrc" "~/.dotfiles/config/bashrc"
+createLink ".xprofile" "~/.xprofile" "~/.dotfiles/config/xprofile"
+createLink ".xsession" "~/.xsession" "~/.dotfiles/config/xsession"
 
+log '--- default apps configs'
 createLink "tmux" "~/.tmux.conf" "~/.dotfiles/config/tmux.config"
 createLink "neovim" "~/.config/nvim" "~/.dotfiles/nvim"
-createLink "fastfetch" "~/.config/fastfetch/config.jsonc" "~/.dotfiles/config/fastfetch.jsonc"
+createLink "fastfetch" "~/.config/fastfetch/config.jsonc" "~/.dotfiles/config/fastfetch.jsonc" 'mkdir -p ~/.config/fastfetch'
+createLink "alacritty" "~/.config/alacritty/alacritty.toml" "~/.dotfiles/config/alacritty.toml" 'mkdir -p ~/.config/alacritty'
+# createLink "wezterm" "~/.config/wezterm/wezterm.lua" "~/.dotfiles/config/wezterm.lua" 'mkdir -p ~/.config/wezterm'
 
-log_warn 'desktop specific configurations'
-
-createLink "wezterm" "~/.config/wezterm/wezterm.lua" "~/.dotfiles/config/wezterm.lua" 'mkdir -p ~/.config/wezterm'
-createLink "waybar" "~/.config/waybar/config.jsonc" "~/.dotfiles/config/waybar.jsonc" 'mkdir -p ~/.config/waybar'
-createLink "waybar style" "~/.config/waybar/style.css" "~/.dotfiles/config/waybar-style.css"
-createLink "hyprland" "~/.config/hyprland/hyprland.conf" "~/.dotfiles/config/hyprland.config" 'mkdir -p ~/.config/hyprland'
+log '--- desktop configs'
+# createLink "waybar" "~/.config/waybar/config.jsonc" "~/.dotfiles/config/waybar.jsonc" 'mkdir -p ~/.config/waybar'
+# createLink "waybar style" "~/.config/waybar/style.css" "~/.dotfiles/config/waybar-style.css"
+# createLink "hyprland" "~/.config/hyprland/hyprland.conf" "~/.dotfiles/config/hyprland.config" 'mkdir -p ~/.config/hyprland'
+# createLink "i3status" "~/.config/i3status/config" "~/.dotfiles/config/i3status.config" 'mkdir -p ~/.config/i3status'
 createLink "i3" "~/.config/i3/config" "~/.dotfiles/config/i3.config" 'mkdir -p ~/.config/i3'
-createLink "i3status" "~/.config/i3status/config" "~/.dotfiles/config/i3status.config" 'mkdir -p ~/.config/i3status'
 createLink "picom" "~/.config/picom/picom.conf" "~/.dotfiles/config/picom.conf" 'mkdir -p ~/.config/picom'
 createLink "rofi" "~/.config/rofi/config.rasi" "~/.dotfiles/config/rofi.css" 'mkdir -p ~/.config/rofi'
 createLink "polybar" "~/.config/polybar/config.ini" "~/.dotfiles/config/polybar.ini" 'mkdir -p ~/.config/polybar'
-createLink "alacritty" "~/.config/alacritty/alacritty.toml" "~/.dotfiles/config/alacritty.toml" 'mkdir -p ~/.config/alacritty'
 
-log_info 'finished'
+log '--- finished'
+
 exit 0
