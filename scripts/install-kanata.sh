b708bb6d10711b0c308527c13129291624b6c23d
Vitor Felix da Silva <vitor_fellix@hotmail.com>
2025-05-27 13:52:34 -0300

    feat: kanata install and service scripts

---
+++ scripts/install-kanata.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+KANATA_CONFIG_PATH="~/.dotfile/kanata/kanata.kbd"
+
+log installing rustup
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+log sourcing /env
+. "$HOME/.cargo/env"
+
+log updating rustup
+rustup update stable
+
+log installing kanata
+cargo install kanata
+
+log running kanata
+kanata --cfg $KANATA_CONFIG_PATH
---
+++ scripts/setup-kanata-service.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# Stop if any error
+set -e
+
+# Source log
+source "/home/vitor/.dotfiles/bin/log"
+
+# Configures the service by linking the configs and removing old ones
+config(){
+	if [[ ! -d "/etc/kanata" ]]; then
+		log "creating directory for the service"
+		mkdir /etc/kanata 
+	fi
+	log "ok directory exists"
+
+	if [[ -e "/etc/kanata/kanata.kbd" ]]; then
+		log "removing existing config"
+		rm "/etc/kanata/kanata.kbd"
+	fi
+	log "creating link for config"
+	ln -s "/home/vitor/.dotfiles/config/kanata.kbd" "/etc/kanata/kanata.kbd"
+
+	if [[ -e "/lib/systemd/system/kanata.service" ]]; then
+		log "removing existing service"
+		rm "/lib/systemd/system/kanata.service"
+	fi
+
+	log "installing service"
+	install -m 644 "/home/vitor/.dotfiles/config/kanata.service" "/lib/systemd/system/kanata.service"
+}
+
+# Starts and prints the status of the service
+start(){
+	systemctl start kanata
+	systemctl status kanata
+}
+
+# Enables the service
+enable(){
+	systemctl daemon-reload
+	systemctl enable kanata
+}
+
+log "configuring service"
+config
+
+log "enabling service"
+enable
+
+log "starting service"
+start
+
