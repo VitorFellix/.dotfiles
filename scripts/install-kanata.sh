3e6c476731c220fcd8db8b6e4ac6d7e3762c6b64
Vitor Felix da Silva <vitor_fellix@hotmail.com>
2025-05-27 13:57:45 -0300

    fix: removed execution of kanata on install-kanata.sh

--- scripts/install-kanata.sh
+++ scripts/install-kanata.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-KANATA_CONFIG_PATH="~/.dotfile/kanata/kanata.kbd"
-
 log "installing rustup"
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
@@ -14,5 +12,4 @@ rustup update stable
 log "installing kanata"
 cargo install kanata
 
-log "running kanata"
-kanata --cfg $KANATA_CONFIG_PATH
+log "kanata installed"
