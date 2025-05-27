fd1c73c5a9e64caaf0adbdd9e5523e63186fb92a
Vitor Felix da Silva <vitor_fellix@hotmail.com>
2025-05-27 13:56:31 -0300

    fix: logs on install-kanata.sh

--- scripts/install-kanata.sh
+++ scripts/install-kanata.sh
@@ -2,17 +2,17 @@
 
 KANATA_CONFIG_PATH="~/.dotfile/kanata/kanata.kbd"
 
-log installing rustup
+log "installing rustup"
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
-log sourcing /env
+log "sourcing /env"
 . "$HOME/.cargo/env"
 
-log updating rustup
+log "updating rustup"
 rustup update stable
 
-log installing kanata
+log "installing kanata"
 cargo install kanata
 
-log running kanata
+log "running kanata"
 kanata --cfg $KANATA_CONFIG_PATH
