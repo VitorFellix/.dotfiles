e04fc9a9000b4d0c42f9d2c4b23096d42acec19e
Vitor Felix da Silva <vitor_fellix@hotmail.com>
2025-05-30 11:42:24 -0300

    update: ssh-keys script changes permission and is compatible with config file

--- scripts/ssh-keys.sh
+++ scripts/ssh-keys.sh
@@ -3,12 +3,22 @@
 ssh_dir=${HOME}/.ssh
 
 if [ ! -d ${ssh_dir} ]; then
-	echo "${ssh_dir} doesnt exists"
+	log "${ssh_dir} doesnt exists"
 	exit 1
 fi
+chmod 700 ~/.ssh
 
-keys=$(find ${ssh_dir} -type f | grep -v "pub\|old\|known\|host\|authorized_keys")
+if [ -d "${ssh_dir}/config" ]; then
+	log "changing ${ssh_dir}/config permissions"
+	chmod 600 "${ssh_dir}/config"
+fi
+
+log "looking for keys on ${ssh_dir}"
+keys=$(find ${ssh_dir} -type f | grep -v "pub\|old\|known\|host\|authorized_keys\|config")
 
 for k in ${keys}; do
+	log "adding key ${k}"
+	chmod 600 "${k}"
+	chmod 644 "${k}.pub"
 	ssh-add ${k}
 done
