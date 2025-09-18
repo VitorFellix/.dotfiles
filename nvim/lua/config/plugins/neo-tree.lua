510fda1cd17ea0a248ecf0a909430162bd82f0e8
Vitor Felix da Silva <vitor_fellix@hotmail.com>
2025-09-18 13:50:34 -0300

    remove: filetree and fixed neo-tree config, added E, A and C keybindings to neo tree.

--- nvim/lua/config/plugins/neo-tree.lua
+++ nvim/lua/config/plugins/neo-tree.lua
@@ -3,15 +3,24 @@ return {
 	branch = "v3.x",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"nvim-tree/nvim-web-devicons",
 		"MunifTanjim/nui.nvim",
-		{ "3rd/image.nvim", opts = {} }, -- Optional image support in preview window: See `# Preview Mode` for more information
+		-- { "3rd/image.nvim", opts = {} }, -- Optional image support in preview window: See `# Preview Mode` for more information
 	},
-	lazy = false, -- neo-tree will lazily load itself
-	---@module "neo-tree"
-	---@type neotree.Config?
+	lazy = false,
 	opts = {
-		-- fill any relevant options here
-        close_if_last_window = true, -- Close Neo-tree if it is the last window left in the tab
+		close_if_last_window = true,
+		window = {
+			mappings = {
+				["A"] = "expand_all_nodes",
+				["E"] = "expand_all_subnodes",
+				["C"] = "close_all_nodes"
+			},
+		},
+		filesystem = {
+			follow_current_file = {
+				enabled = true,
+			},
+		},
 	},
 }
