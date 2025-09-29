91fe6dd9adc283d8fa4912287308f3677b44bfc1
Vitor Felix da Silva <vitor_fellix@hotmail.com>
2025-09-28 22:37:53 -0300

    fix: metals config... not sure if it fixed but its better, as it shows the messages now. Metals auto starts and doesnt needs nvim-lsp so if you start with nvim-lsp it will crash

--- nvim/lua/config/plugins/metals.lua
+++ nvim/lua/config/plugins/metals.lua
@@ -1,45 +1,57 @@
 return {
 	"scalameta/nvim-metals",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		{
-			"mfussenegger/nvim-dap",
-			config = function(self, opts)
-				local dap = require("dap")
-
-				dap.configurations.scala = {
-					{
-						type = "scala",
-						request = "launch",
-						name = "RunOrTest",
-						metals = {
-							runType = "runOrTestFile",
-						},
-					},
-					{
-						type = "scala",
-						request = "launch",
-						name = "Test Target",
-						metals = {
-							runType = "testTarget",
-						},
-					},
-				}
-			end,
-		},
-	},
 	ft = { "scala", "sbt" },
+    dependencies = {
+      {
+        "j-hui/fidget.nvim",
+        opts = {},
+      },
+      {
+        "mfussenegger/nvim-dap",
+        config = function(self, opts)
+          -- Debug settings if you're using nvim-dap
+          local dap = require("dap")
+
+          dap.configurations.scala = {
+            {
+              type = "scala",
+              request = "launch",
+              name = "RunOrTest",
+              metals = {
+                runType = "runOrTestFile",
+                --args = { "firstArg", "secondArg", "thirdArg" }, -- here just as an example
+              },
+            },
+            {
+              type = "scala",
+              request = "launch",
+              name = "Test Target",
+              metals = {
+                runType = "testTarget",
+              },
+            },
+          }
+        end
+      },
+    },
 	opts = function()
 		local metals_config = require("metals").bare_config()
 		metals_config.capabilities = require("cmp_nvim_lsp").default_capabilities()
 		metals_config.on_attach = function(client, bufnr)
 			require("metals").setup_dap()
 		end
-		metals_config.init_options.statusBarProvider = "on"
+		metals_config.init_options.statusBarProvider = "off"
+
+		metals_config.settings = {
+      	  showImplicitArguments = true,
+      	  excludedPackages = { "akka.actor.typed.javadsl", "com.github.swagger.akka.javadsl" },
+      	}
+
 		return metals_config
 	end,
 	config = function(self, metals_config)
 		local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
+
 		vim.api.nvim_create_autocmd("FileType", {
 			pattern = self.ft,
 			callback = function()
