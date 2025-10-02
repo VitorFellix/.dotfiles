c731252306be8da14dda512308e86f8712b534f1
Vitor Felix da Silva <vitor_fellix@hotmail.com>
2025-10-01 23:17:33 -0300

    remove: monokai and kanagawa files

--- nvim/lua/config/plugins/web-devicons.lua
+++ nvim/lua/config/plugins/web-devicons.lua
@@ -3,14 +3,14 @@ return {
 		-- your personnal icons can go here (to override)
 		-- you can specify color or cterm_color instead of specifying both of them
 		-- DevIcon will be appended to `name`
-		override = {
-			zsh = {
-				icon = "",
-				color = "#428850",
-				cterm_color = "65",
-				name = "Zsh"
-			}
-		},
+		-- override = {
+		-- 	zsh = {
+		-- 		icon = "",
+		-- 		color = "#428850",
+		-- 		cterm_color = "65",
+		-- 		name = "Zsh"
+		-- 	}
+		-- },
 		-- globally enable different highlight colors per icon (default to true)
 		-- if set to false all icons will have the default icon's color
 		color_icons = true,
@@ -24,7 +24,7 @@ return {
 		strict = true,
 		-- set the light or dark variant manually, instead of relying on `background`
 		-- (default to nil)
-		variant = "dark",
+		-- variant = "dark",
 		-- same as `override` but specifically for overrides by filename
 		-- takes effect when `strict` is true
 		override_by_filename = {
