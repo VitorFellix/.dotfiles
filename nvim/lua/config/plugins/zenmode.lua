b4750678fc62bf2acba8fd52de75f67ddad3b7c3
Vitor Felix da Silva <vitor_fellix@hotmail.com>
2025-10-05 22:06:24 -0300

    fix: zenmode options

--- nvim/lua/config/plugins/zenmode.lua
+++ nvim/lua/config/plugins/zenmode.lua
@@ -1,16 +1,19 @@
 return {
-	'folke/zen-mode.nvim',
-	config = function()
-		require 'zen-mode'.setup({
-			opts = {
-				window = {
-					options = {
-						number = true,
-						relativenumber = true,
-						signcolumn = 'yes',
-					}
-				},
+	"folke/zen-mode.nvim",
+	opts = {
+		window = {
+			backdrop = 1, -- shade the backdrop of the Zen window. Set to 1 to keep the same as Normal
+			width = 120, -- width of the Zen window
+			height = 0.7, -- height of the Zen window
+			options = {
+				signcolumn = "no",
+				number = true,
+				relativenumber = false,
+				cursorline = true,
+				cursorcolumn = false,
+				foldcolumn = "0",
+				list = false,
 			},
-		})
-	end
+		},
+	},
 }
