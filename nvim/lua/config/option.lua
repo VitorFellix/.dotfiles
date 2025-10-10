cd9baf005d472ac4aac64aa7c3d2d91a81e3a3b2
Vitor Felix da Silva <vitor_fellix@hotmail.com>
2025-10-09 22:35:48 -0300

    update: options

--- nvim/lua/config/option.lua
+++ nvim/lua/config/option.lua
@@ -5,23 +5,28 @@ vim.cmd("syntax off")
 vim.cmd("colorscheme kanagawa-wave")
 
 vim.opt.number = true
-vim.opt.relativenumber = false
+vim.opt.relativenumber = true
+vim.opt.numberwidth = 6
 
 vim.opt.clipboard = "unnamedplus"
 
-vim.opt.smartcase = true
 vim.opt.ignorecase = true
+vim.opt.smartcase = true
 
-vim.opt.wrap = false
+vim.opt.wrap = true
 vim.opt.linebreak = true
 vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 0
+vim.opt.shiftwidth = 0
 vim.opt.smartindent = true
 vim.opt.breakindent = true
+vim.opt.cmdheight = 1
+vim.opt.showmode = false
+vim.opt.showtabline = 0
+vim.opt.expandtab = true
 
-vim.opt.signcolumn = "yes"
-vim.opt.scrolloff = 12
+vim.opt.signcolumn = "no"
+vim.opt.scrolloff = 3
 vim.opt.colorcolumn = "120"
 vim.opt.termguicolors = true
 vim.opt.cursorline = true
@@ -33,17 +38,26 @@ vim.opt.showmatch = true
 vim.opt.completeopt = { "menuone", "noselect" }
 
 vim.opt.inccommand = "split"
+
 vim.opt.splitright = true
 vim.opt.splitbelow = true
+
+vim.opt.ruler = true
 
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.writebackup = false
+vim.opt.undofile = true
+vim.opt.undodir = vim.fn.stdpath("data") .. "/undo"
 
-vim.opt.laststatus = 3
+vim.opt.updatetime = 100
+vim.opt.laststatus = 0
 
 vim.opt.title = true
 vim.opt.titlestring = "%t"
+
+vim.opt.winborder = "bold"
+vim.opt.cursorlineopt = "both"
 
 vim.opt.encoding = "utf-8"
 vim.opt.fileencoding = "utf-8"
