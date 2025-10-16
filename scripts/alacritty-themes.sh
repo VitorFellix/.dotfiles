f50735889d5fd67490c01c8f3929a5e0de544d37
Vitor Felix da Silva <vitor_fellix@hotmail.com>
2025-10-16 13:47:38 -0300

    feat: alacritty-themes.

---
+++ scripts/alacritty-themes.sh
@@ -0,0 +1,8 @@
+mkdir -p ~/.config/alacritty/themes
+git clone https://github.com/alacritty/alacritty-theme ~/.config/alacritty/themes
+
+# Change on the config
+# [general]
+# import = [
+#     "~/.config/alacritty/themes/themes/{theme}.toml"
+# ]
